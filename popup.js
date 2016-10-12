462a6ee8cdcdcd6dc6895523568d244dab2c3c3d
Adam Bradley <adam_bradley@brown.edu>
2016-10-12 08:57:56 -0400

    Updating config variable names to reduce chances of collisions.

--- popup.js
+++ popup.js
@@ -10,8 +10,8 @@ function setStatus(newstat) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('logo').setAttribute('src', logo_image);
-    setStatus(status_start);
+    document.getElementById('logo').setAttribute('src', ocra_logo_image);
+    setStatus(ocra_status_start);
 
     chrome.tabs.executeScript({file: 'content.js'}, function(data) {
         if ( data[0].length > 0 ) {
@@ -21,21 +21,21 @@ document.addEventListener('DOMContentLoaded', function() {
                 lst = el.getElementsByTagName('ul')[0];
                 if ( 'page' == m.type ) {
                     document.getElementById('thispage').removeAttribute('hidden');
-                    lnkurl = target_page+'?'+m.clas+'='+m.match;
+                    lnkurl = ocra_target_page+'?'+m.clas+'='+m.match;
                     lnk = '<b>'+m.label+':</b> <a href="'+lnkurl+'">'+m.match+'</a>';
                 } else {
                     document.getElementById('otherids').removeAttribute('hidden');
-                    lnkurl = target_page+'?'+m.type+'='+m.match;
+                    lnkurl = ocra_target_page+'?'+m.type+'='+m.match;
                     lnk = '<a href="'+lnkurl+'">'+m.match+'</a>';
                 }
                 lst.innerHTML += "<li>"+lnk+"</li>\n";
             });
 
-            setStatus(status_ready);
+            setStatus(ocra_status_ready);
             $('#tabName').blur();
         } else {
             //Shouldn't happen. There's always the page URL as a last resort.
-            setStatus(status_nothing_to_do);
+            setStatus(ocra_status_nothing_to_do);
         }
     });
 });
